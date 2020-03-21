f9e9be074a4d7a1c468254e10dab5249c1974ffc
jgersain <jhersein@gmail.com>
2020-03-21 00:09:46 -0600

    Added simple modal component.

---
+++ gersain_app/src/components/Modal.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  render() {
+    return (this.show) ? <div class="modal">
+      <div class="modal-header">
+        <a href="#" onClick={this.onClose}>Close</a>
+      </div>
+      <div class="modal-content">
+        { this.$slots.default }
+        <button onClick={ this.clickOkHandler }>Aceptar</button>
+        <button onClick={ this.clickCancelHandler }>Cancelar</button>
+      </div>
+    </div> : null;
+  },
+  methods: {
+    clickOkHandler: () => alert('Ok'),
+    clickCancelHandler: () => alert('Cancel'),
+    onClose() { this.$emit('close-modal'); },
+  },
+};
+</script>
