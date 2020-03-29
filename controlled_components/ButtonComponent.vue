d3b10ded969997235f7ad1751beac0e4995cfb80
jgersain <jhersein@gmail.com>
2020-03-29 13:08:30 -0600

    Button component created.

---
+++ controlled_components/ButtonComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="bg-teal-400 w-full p-3 text-white rounded shadow hover:bg-teal-500"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComponent',
+}
+</script>
